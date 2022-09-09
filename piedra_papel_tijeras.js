2a36a7740a39aeaadefdfd8b98acf7217e6687f0
Aldo Mejia <aldonear@gmail.com>
2022-09-09 01:14:44 -0500

    Guarde el combate dentro de una funcion y la mande llamar dentro del while

--- piedra_papel_tijeras.js
+++ piedra_papel_tijeras.js
@@ -20,35 +20,40 @@ function eleccion(jugada){
 }
 
 //Creamos una funcion para guardar el combate.
+function combate(){
+    //COMBATE
+    if(pc == jugador){
+        resultado__combate = "¡¡EMPATE!!";
+        empates ++;
+    }else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
+        resultado__combate = "¡¡GANASTE!!";
+        triunfos ++;
+    }else{
+        resultado__combate = "¡¡PERDISTE!!";
+        perdidas ++;
+    }
 
+    alert(resultado__combate);
+}
 
 // 1 es piedra, 2 es papel, 3 es tijeras. 
 let jugador = 0;
 let pc = 0;
 let triunfos = 0;
 let perdidas = 0;
-
+let empates = 0;
 
 while(triunfos < 3 && perdidas < 3){
-     pc = aleatorio(1, 3);
+     
+    pc = aleatorio(1, 3);
 
     jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijeras");
     // alert("Elegiste " + jugador);
 
     alert("Tu eliges: " + eleccion(jugador));
     alert("PC elige: " + eleccion(pc));
-
-    //COMBATE
-
-    if(pc == jugador){
-        alert("¡¡EMPATE!!");
-    }else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
-        alert("¡¡GANASTE!!");
-        triunfos ++;
-    }else{
-        alert("¡¡PERDISTE!!");
-        perdidas ++;
-    }   
+    
+    combate();
 }
 
-alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces.");
+alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces. Empataste " + empates + " veces.");
